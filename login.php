bc06d80cd93417c66690377f84e3aeb5d8f48fa6
Jenish Limbu <jlimbu539@gmail.com>
2024-05-11 23:05:38 +0545

    login and registraation form design changed

--- login.php
+++ login.php
@@ -11,43 +11,46 @@ session_start();
   <title>Daily Activities & Personal Finance Tracker</title>
   <link rel="stylesheet" href="./style.css">
 </head>
-<div class="p-5">
-  <form action="" method="post" id="loginForm">
-    <h2 style="text-align:center">Login Form</h2>
 
-    <div><label for="">Email</label> <input type="text" name="email" /></div>
-    <div><label for="">Password</label> <input name="password" type="text" /></div>
-    <div><button name="login">Login</button></div>
-    <div>
-      or <a href="./registration.php">register?</a>
-    </div>
-  </form>
-</div>
-<?php
-include("connection.php");
-$tbl_name = "users";
-try {
-  if (isset($_POST["login"])) {
-    $email = $_POST["email"];
-    $password = $_POST["password"];
-    $selectQuery = "SELECT id, email, password,usertype FROM $tbl_name where email='$email' && password='$password'";
-    $execute = mysqli_query($conn, $selectQuery);
-    $fetchedData = mysqli_fetch_assoc($execute);
-    if ($fetchedData['email'] === $email && $fetchedData['password'] === $password && $fetchedData['usertype'] == "user") {
-      $_SESSION['logged_in'] = "true";
-      $_SESSION['user_id'] = $fetchedData['id'];
-      header("location:./userpanel/dashboard.php");
-      $_SESSION['fullname'] = $fetchedData['fullname'];
-    } else if ($fetchedData['email'] === $email && $fetchedData['password'] === $password && $fetchedData['usertype'] == "admin") {
-      $_SESSION['user_id'] = $fetchedData['id'];
-      header("location:./adminpanel/dashboard.php");
-    } else {
-      echo "login failed";
+<body style="background-color: #adadad; display:flex; height:100vh; align-items:center; justify-content:center">
+  <div class="p-5">
+    <form action="" method="post" id="loginForm">
+      <h2 style="text-align:center">Login</h2>
+
+      <div><label for="">Email</label> <input type="text" name="email" /></div>
+      <div><label for="">Password</label> <input name="password" type="text" /></div>
+      <div><button name="login">Login</button></div>
+      <div>
+        or <a href="./registration.php">register?</a>
+      </div>
+    </form>
+  </div>
+  <?php
+  include("connection.php");
+  $tbl_name = "users";
+  try {
+    if (isset($_POST["login"])) {
+      $email = $_POST["email"];
+      $password = $_POST["password"];
+      $selectQuery = "SELECT id, email, password,usertype FROM $tbl_name where email='$email' && password='$password'";
+      $execute = mysqli_query($conn, $selectQuery);
+      $fetchedData = mysqli_fetch_assoc($execute);
+      if ($fetchedData['email'] === $email && $fetchedData['password'] === $password && $fetchedData['usertype'] == "user") {
+        $_SESSION['logged_in'] = "true";
+        $_SESSION['user_id'] = $fetchedData['id'];
+        header("location:./userpanel/dashboard.php");
+        $_SESSION['fullname'] = $fetchedData['fullname'];
+      } else if ($fetchedData['email'] === $email && $fetchedData['password'] === $password && $fetchedData['usertype'] == "admin") {
+        $_SESSION['user_id'] = $fetchedData['id'];
+        header("location:./adminpanel/dashboard.php");
+      } else {
+        echo "login failed";
+      }
     }
+  } catch (Exception $e) {
+    echo $e->getMessage();
   }
-} catch (Exception $e) {
-  echo $e->getMessage();
-}
-?>
+  ?>
+</body>
 
 </html>
--- registration.php
+++ registration.php
@@ -7,30 +7,34 @@
   <title>Daily Activities & Personal Finance Tracker</title>
   <link rel="stylesheet" href="./style.css">
 </head>
-<div class="p-5">
-  <form action="" method="post" id="registrationForm">
-    <h2 style="text-align: center;">Registration Form</h2>
-    <div><label for="">Full Name</label> <input type="text" name="fullname" required /></div>
-    <div><label for="">Email</label> <input name="email" type="email" required /></div>
-    <div><label for="">Password</label> <input name="password" type="password" required /></div>
-    <div><button name="register">Registration</button></div>
-    <div>
-      or <a href="./login.php">Login</a>
-    </div>
-  </form>
 
-</div>
-<?php
-include("connection.php");
-if (isset($_POST['register'])) {
-  $fullname = $_POST['fullname'];
-  $email = $_POST['email'];
-  $password = $_POST['password'];
-  $insertQuery = "INSERT INTO users(fullname, email, password) VALUES ('$fullname', '$email', '$password') ";
-  $response = mysqli_query($conn, $insertQuery);
-  header("location:./login.php");
-}
+<body style="background-color: #adadad; display:flex; height:100vh; align-items:center; justify-content:center">
 
-?>
+  <div class="p-5">
+    <form action="" method="post" id="registrationForm">
+      <h2 style="text-align: center;">Registration</h2>
+      <div><label for="">Full Name</label> <input type="text" name="fullname" required /></div>
+      <div><label for="">Email</label> <input name="email" type="email" required /></div>
+      <div><label for="">Password</label> <input name="password" type="password" required /></div>
+      <div><button name="register">Registration</button></div>
+      <div>
+        or <a href="./login.php">Login?</a>
+      </div>
+    </form>
+
+  </div>
+  <?php
+  include("connection.php");
+  if (isset($_POST['register'])) {
+    $fullname = $_POST['fullname'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $insertQuery = "INSERT INTO users(fullname, email, password) VALUES ('$fullname', '$email', '$password') ";
+    $response = mysqli_query($conn, $insertQuery);
+    header("location:./login.php");
+  }
+
+  ?>
+</body>
 
 </html>
